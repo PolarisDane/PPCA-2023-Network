5b8168b778dfd72b56832629a5256382a35fb7b3
PolarisDane <2488721971@qq.com>
2023-07-20 15:49:58 +0800

    Completed HTTP modify along with HTTPS modify, though problems exist with gzip Basic tasks completed

--- Socks5 Proxy Server/src/CONNECTHandler.go
+++ Socks5 Proxy Server/src/CONNECTHandler.go
@@ -12,28 +12,15 @@ import (
 
 func Forward(tarConn, srcConn net.Conn) {
 	buf := make([]byte, 32*1024)
-	HTTPtag := -1
 	for {
 		rcount, err := srcConn.Read(buf[:32*1024])
 		if err != nil {
 			fmt.Println(err.Error())
 			return
 		}
-		if HTTPtag == -1 {
-			HTTPtag = Is_HTTP_Request(buf, rcount)
-			if HTTPtag == 0 {
-				fmt.Println("HTTP GET")
-			} else if HTTPtag == 1 {
-				fmt.Println("HTTP POST")
-			} else if HTTPtag == 2 {
-				fmt.Println("HTTP CONNECT")
-			} else {
-				fmt.Println("NOT HTTP")
-			}
-		}
 		if rcount > 0 {
 			wcount, err := tarConn.Write(buf[:rcount])
-			fmt.Println(string(buf[:rcount]))
+			//fmt.Println(string(buf[:rcount]))
 			if err != nil {
 				fmt.Println(err.Error())
 				return
@@ -49,8 +36,6 @@ func Forward(tarConn, srcConn net.Conn) {
 func HandleConnect(tar string, clientConn net.Conn) {
 	fmt.Println("CONNECT success!!!")
 	tarConn, err := net.DialTimeout("tcp", tar, 3*time.Second)
-	defer clientConn.Close()
-	defer tarConn.Close()
 	//与目标连接
 	if err != nil {
 		if strings.Contains(err.Error(), "connection refused") {
@@ -65,80 +50,78 @@ func HandleConnect(tar string, clientConn net.Conn) {
 	}
 	AnswerRequest(clientConn, 0)
 	//回复代理请求
-	reader := bufio.NewReader(clientConn)
-	line, err := reader.Peek(8)
-	if err != nil {
-		fmt.Println("Internal server error" + err.Error())
-		return
-	}
-	if (line[0] == 0x16) && (line[1] == 0x03) && (line[2] == 0x01) && TLS_Hijack {
-		tarConn.Close()
-		Listener, err := net.Listen("tcp", ":0")
+	if TLS_Hijack {
+		reader := bufio.NewReader(clientConn)
+		line, err := reader.Peek(8)
 		if err != nil {
-			fmt.Println(err.Error())
+			fmt.Println("Internal server error" + err.Error())
 			return
 		}
-		handshakeAddr := Listener.Addr().String()
-		fmt.Println("Listening for TLS handshake")
-		go func() {
-			handshakeConn, err := Listener.Accept()
+		if (line[0] == 0x16) && (line[1] == 0x03) && (line[2] == 0x01) {
+			defer clientConn.Close()
+			tarConn.Close()
+			Listener, err := net.Listen("tcp", ":0")
 			if err != nil {
 				fmt.Println(err.Error())
 				return
 			}
-			cert, err := generateCert(tar)
-			if err != nil {
-				fmt.Println(err.Error())
-				return
-			}
-			tlsConfig := &tls.Config{
-				Certificates:       []tls.Certificate{cert},
-				InsecureSkipVerify: true,
-			}
-			tlsConn := tls.Server(handshakeConn, tlsConfig)
-			err = tlsConn.Handshake()
-			if err != nil {
-				fmt.Println(err.Error())
-				return
-			}
-			fmt.Println("Received TLS handshake")
+			handshakeAddr := Listener.Addr().String()
+			fmt.Println("Listening for TLS handshake")
+			go func() {
+				handshakeConn, err := Listener.Accept()
+				if err != nil {
+					fmt.Println(err.Error())
+					return
+				}
+				cert, err := generateCert(tar)
+				if err != nil {
+					fmt.Println(err.Error())
+					return
+				}
+				tlsConfig := &tls.Config{
+					Certificates:       []tls.Certificate{cert},
+					InsecureSkipVerify: true,
+				}
+				tlsConn := tls.Server(handshakeConn, tlsConfig)
+				err = tlsConn.Handshake()
+				if err != nil {
+					fmt.Println(err.Error())
+					return
+				}
+				fmt.Println("Received TLS handshake")
 
-			tlsConfig = &tls.Config{
-				InsecureSkipVerify: true,
-			}
-			serverConn, err := tls.Dial("tcp", tar, tlsConfig)
+				tlsConfig = &tls.Config{
+					InsecureSkipVerify: true,
+				}
+				serverConn, err := tls.Dial("tcp", tar, tlsConfig)
+				if err != nil {
+					fmt.Println(err.Error())
+					return
+				}
+				defer serverConn.Close()
+				go ModifyForward(tlsConn, serverConn)
+				io.Copy(serverConn, tlsConn)
+			}()
+			proxyConn, err := net.DialTimeout("tcp", handshakeAddr, 3*time.Second)
 			if err != nil {
 				fmt.Println(err.Error())
 				return
 			}
-			defer serverConn.Close()
-			go Forward(tlsConn, serverConn)
-			io.Copy(serverConn, tlsConn)
-		}()
-		proxyConn, err := net.DialTimeout("tcp", handshakeAddr, 3*time.Second)
-		if err != nil {
-			fmt.Println(err.Error())
-			return
+			defer proxyConn.Close()
+			var buf [32 * 1024]byte
+			go io.Copy(clientConn, proxyConn)
+			n, err := reader.Read(buf[:])
+			if err != nil {
+				fmt.Println(err.Error())
+				return
+			}
+			proxyConn.Write(buf[:n])
+			io.Copy(proxyConn, clientConn)
 		}
-		defer proxyConn.Close()
-		var buf [32 * 1024]byte
-		go io.Copy(clientConn, proxyConn)
-		n, err := reader.Read(buf[:])
-		if err != nil {
-			fmt.Println(err.Error())
-			return
-		}
-		proxyConn.Write(buf[:n])
-		io.Copy(proxyConn, clientConn)
 	} else {
-		go io.Copy(clientConn, tarConn)
-		var buf [32 * 1024]byte
-		n, err := reader.Read(buf[:])
-		if err != nil {
-			fmt.Println(err.Error())
-			return
-		}
-		tarConn.Write(buf[:n])
-		Forward(tarConn, clientConn)
+		defer clientConn.Close()
+		defer tarConn.Close()
+		go ModifyForward(clientConn, tarConn)
+		io.Copy(tarConn, clientConn)
 	}
 }
--- Socks5 Proxy Server/src/HTTP.go
+++ Socks5 Proxy Server/src/HTTP.go
@@ -13,6 +13,9 @@ func ModifyForward(tarConn, srcConn net.Conn) {
 	data := make([]byte, 0)
 	buf := make([]byte, 32*1024)
 	tag := -1
+	lens := 0
+	index := -1
+	contentLength := int64(-1)
 	for {
 		rcount, err := srcConn.Read(buf[:32*1024])
 		if err != nil {
@@ -20,12 +23,84 @@ func ModifyForward(tarConn, srcConn net.Conn) {
 			return
 		}
 		if tag == -1 {
-
+			tag = IsResponse(buf, rcount)
+		}
+		if tag > 0 {
+			lens += rcount
+			data = append(data, buf[:rcount]...)
+			if index == -1 {
+				index = strings.Index(string(data), "\r\n\r\n")
+				//找到响应头
+				if index != -1 {
+					contentLength = ResponseParse(data)
+					if contentLength == -2 {
+						return
+					}
+				}
+			}
+			if index != -1 {
+				if contentLength == -1 {
+					//内容长度不确定，如使用chunked
+					if strings.HasSuffix(string(data[lens-5:lens]), "0\r\n\r\n") {
+						//chunked传输的结尾为0\r\n\r\n
+						data, lens = ResponseDecode(data, lens)
+						if lens == -1 {
+							return
+						}
+						tag = -1
+					}
+				} else {
+					if lens == index+int(contentLength)+4 {
+						tag = -1
+					}
+				}
+			}
+			if tag == -1 {
+				data, lens = ResponseModify(data, lens)
+				wcount, err := tarConn.Write(data[:lens])
+				//fmt.Println(string(data[:lens]))
+				if err != nil {
+					fmt.Println(err.Error())
+					return
+				}
+				if wcount < lens {
+					fmt.Println("Package end")
+					return
+				}
+				data = data[:0]
+				lens = 0
+				index = -1
+				contentLength = -1
+			}
+		} else {
+			if rcount > 0 {
+				wcount, err := tarConn.Write(buf[:rcount])
+				//fmt.Println(string(buf[:rcount]))
+				if err != nil {
+					fmt.Println(err.Error())
+					return
+				}
+				if wcount < rcount {
+					fmt.Println("End of forward")
+					return
+				}
+			}
 		}
 	}
 }
 
-func Is_HTTP_Request(buf []byte, count int) int {
+func IsResponse(buf []byte, count int) int {
+	if count > 8 {
+		count = 8
+	}
+	if strings.HasPrefix(string(buf[:count]), "HTTP") {
+		return 1
+	} else {
+		return 0
+	}
+}
+
+func IsRequest(buf []byte, count int) int {
 	if count > 8 {
 		count = 8
 	}
@@ -50,43 +125,62 @@ func Is_HTTP_Request(buf []byte, count int) int {
 	}
 }
 
-func Http_Response_Parse(buf []byte, n int) int64 {
+func ResponseParse(buf []byte) int64 {
 	resp, err := http.ReadResponse(bufio.NewReader(bytes.NewBuffer(buf)), nil)
 	if err != nil {
-		return -1
+		fmt.Println(err.Error())
+		return -2
 	}
 	return resp.ContentLength
 }
 
-func Http_Response_Modify(buf []byte, n int) ([]byte, int) {
+func ResponseDecode(buf []byte, count int) ([]byte, int) {
+	str := string(buf)
+	index := strings.Index(str, "\r\n\r\n") + 4
+	data := make([]byte, 0)
+	head := make([]byte, 0)
+	head = append(head, str[:index]...)
+	for {
+		siz := 0
+		fmt.Sscanf(str[index:], "%x", &siz)
+		if siz == 0 {
+			break
+		}
+		index += strings.Index(str[index:], "\r\n") + 2
+		data = append(data, str[index:index+siz]...)
+		index += siz + 2
+	}
+	head = append(head, data...)
+	return head, 0
+} //将chunked方式传输的数据拼接
+
+func ResponseModify(buf []byte, n int) ([]byte, int) {
 	index := strings.Index(string(buf), "\r\n\r\n")
-	fmt.Printf("Origin2:\n%v\n", string(buf))
+	//fmt.Printf("Origin2:\n%v\n", string(buf))
 	data := make([]byte, 0)
 	data = append(data, buf[index:]...)
 
 	// Here, do something to data.
-	// data = bytes.Replace(data, []byte("努力"), []byte("天天摆烂"), -1)
+	data = bytes.Replace(data, []byte("百度"), []byte("Polaris_Dane"), -1)
 
-	head := Http_Response_Modify_Head(string(buf[:index]), len(data)-4)
+	head := ResponseHeadModify(string(buf[:index]), len(data)-4)
 
 	head = append(head, data...)
-	fmt.Printf("Final2:\n%v\n", string(head))
-	fmt.Println("Compare", len(head)-len(buf))
+	//fmt.Printf("Final2:\n%v\n", string(head))
+	//fmt.Println("Compare", len(head)-len(buf))
 
 	return head, len(head)
 }
 
-func Http_Response_Modify_Head(str string, n int) []byte {
+func ResponseHeadModify(str string, n int) []byte {
 	slice := strings.Split(str, "\r\n")
 	target := "Content-Length: " + fmt.Sprintf("%d", n)
 	for i := 0; i < len(slice); i++ {
 		if strings.HasPrefix(slice[i], "Content-Length:") {
-			fmt.Println("Code:", slice[i])
 			slice[i] = target
 			break
 		}
 		if strings.HasPrefix(slice[i], "Transfer-Encoding: chunked") {
-			fmt.Println("Code:", slice[i])
 			slice[i] = target
 			break
 		}
